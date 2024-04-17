3a85c036339c173c7c3c6b49cda1ba7b253d395b
emmanuel.bocquillon <emmanuel.bocquillon@sia-partners.com>
2024-04-17 11:35:18 +0200

    set secured db connection

--- tasks/scraper/src/scraper.py
+++ tasks/scraper/src/scraper.py
@@ -16,9 +16,6 @@ def scrape_data():
         URL_ARRETES = 'https://www.data.gouv.fr/fr/datasets/r/782aac32-29c8-4b66-b231-ab4c3005f574'
         df_arretes = get_csv_file(URL_ARRETES)
         
-        # URL_DEPARTEMENTS = 'https://www.data.gouv.fr/fr/datasets/r/90b9341a-e1f7-4d75-a73c-bbc010c7feeb'
-        # gdf_departements = get_departements(URL_DEPARTEMENTS)
-
         # Connect to PostgreSQL
         engine = create_engine(os.environ['DATABASE_URL'])
 
@@ -26,26 +23,6 @@ def scrape_data():
         df_zones.to_sql('zones', con=engine, if_exists='replace', index=False)
         df_arretes.to_sql('arretes', con=engine, if_exists='replace', index=False)
         
-        # with engine.connect() as conn:
-        #     command = text("""CREATE EXTENSION postgis;""")
-        #     conn.execute(command)
-            
-            # command = text("""DROP TABLE IF EXISTS public.departements;
-            #                     CREATE TABLE IF NOT EXISTS public.departements (
-            #                     geometry geometry(Geometry, 4326),
-            #                     code TEXT,
-            #                     nom TEXT
-            #                 );
-            #                 """)
-            # conn.execute(command)
-        # gdf_departements.to_postgis('departements', engine, index=False)
-        
-        # Ensure GeoPandas can handle the 'geometry' column
-        # gdf_departements['geometry'] = gdf_departements['geometry'].apply(lambda x: x.wkt if x is not None else None)
-
-        # Store GeoPandas dataframe into PostgreSQL
-        # gdf_departements[:10].to_postgis('departements', engine, if_exists='replace', index=False)
-
         return jsonify({"success": "Data scraped and stored in database with success"}), 200
     
     except Exception as e:
